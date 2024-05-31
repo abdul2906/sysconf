3ed191ece5ce31216df39e78b1a0ffdd2f7a3b2c
caem <me@caem.dev>
2024-06-01 00:21:25 +0200

    Disable the default branch warning in git
    "master" sounds way cooler than "main". Haters are going to hate.

--- users/hu/packages/git.nix
+++ users/hu/packages/git.nix
@@ -3,11 +3,16 @@
 {
   programs.git = {
     enable = true;
+
     userName = "caem";
     userEmail = "me@caem.dev";
     signing = {
       key = "E50FC66B5062070DC462661C69A830D03203405F";
       signByDefault = true;
+    };
+
+    extraConfig = {
+      init.defaultBranch = "master";
     };
   };
 }
