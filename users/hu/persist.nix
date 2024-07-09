4003cc3d61fd2c1a366485fbb6e0378f14800b8c
caem <me@caem.dev>
2024-07-09 14:04:31 +0200

    Store default data for firefox in persistence. I eventually want to modify this to automatically add only the extension data by gettin the files through globbing or something but I don't know how to do that yet and it's not really that high of a priority as firefox will clean those either way on restart and it'd be more of a keeping everything tidy problem.

--- users/hu/persist.nix
+++ users/hu/persist.nix
@@ -22,6 +22,7 @@
       ".local/share/direnv"
       ".mozilla/firefox/shaga/bookmarkbackups"
       ".mozilla/firefox/shaga/chrome"
+      ".mozilla/firefox/shaga/storage/default"
       ".config/Signal"
       ".config/vesktop"
       ".config/tutanota-desktop"
