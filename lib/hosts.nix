0898c67a5a0c7b007a98b68ce220a67bffa14a35
caem <me@caem.dev>
2024-07-25 21:32:55 +0200

    Simplify mkHosts

--- lib/hosts.nix
+++ lib/hosts.nix
@@ -5,20 +5,12 @@
     modules,
     nixpkgs,
     inputs,
-    path ? (builtins.toString ../hosts),
-  }: let
-    hosts = lib.getDirsInDir path;
-    common = {
-      system = "x86_64-linux";
-      modules = modules;
-    };
-  in
-    builtins.listToAttrs (builtins.map (host: {
+  }: builtins.listToAttrs (builtins.map (host: {
       name = host;
       value = nixpkgs.lib.nixosSystem {
-        system = common.system;
-        modules = common.modules ++ [ ../hosts/${host} ];
+        system = "x86_64-linux";
+        modules = modules ++ [ ../hosts/${host} ];
         specialArgs = { inherit inputs; };
       };
-    }) hosts);
+    }) (lib.getDirsInDir ../hosts));
 }
