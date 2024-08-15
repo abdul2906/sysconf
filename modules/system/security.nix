d4190d1f7d1a1b01d6bad3ca198d77a5c37ab302
caem <me@caem.dev>
2024-08-15 13:49:02 +0200

    Remove clamav as it is broken right now

--- modules/system/security.nix
+++ modules/system/security.nix
@@ -5,13 +5,6 @@
     Defaults  lecture="never"
   '';
 
-  services.clamav = {
-    scanner.enable = true;
-    daemon.enable = true;
-    fangfrisch.enable = true;
-    updater.enable = true;
-  };
-
   security.apparmor = {
     enable = true;
   };
