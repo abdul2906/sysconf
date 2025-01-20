c96f68e5c2ee025e6eecda04045b5dae448d928e
caem <me@caem.dev>
2025-01-20 22:45:14 +0100

    testing dconf stuff

--- modules/home/caem/desktop/gnome/default.nix
+++ modules/home/caem/desktop/gnome/default.nix
@@ -1,5 +1,19 @@
 { ... }:
 
 {
-
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      accent-color = "teal";
+      color-scheme = "prefer-dark";
+    };
+    "org/gnome/desktop/input-sources" = {
+      sources = "[(gb, de)]";
+    };
+    "org/gnome/desktop/peripherals/mouse" = {
+      accel-profile = "flat";
+    };
+    "org/gnome/shell" = {
+      favorite-apps = "['com.mitchellh.ghostty','org.gnome.Nautilus']";
+    };
+  };
 }
