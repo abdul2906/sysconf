e935441b80a2404c09304680a7c3dcd9f1a8d20d
caem <me@caem.dev>
2025-02-01 20:59:52 +0100

    art: Add opentabletdriver

--- modules/nixos/multimedia/art/default.nix
+++ modules/nixos/multimedia/art/default.nix
@@ -5,11 +5,17 @@
     users."${username}".directories = [
       ".local/share/krita"
       ".config/GIMP"
+      ".config/OpenTabletDriver"
     ];
   };
-  
+
   environment.systemPackages = with pkgs; [
     krita
     gimp
   ];
+
+  hardware.opentabletdriver = {
+    enable = true;
+    daemon.enable = true;
+  };
 }
