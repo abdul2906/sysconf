0b1c74693cb475c27bff2594d0823b78a5f2764a
caem <me@caem.dev>
2025-02-02 19:11:59 +0100

    steam: Fix the steam library being inaccesible to steam

--- overlays/steam.nix
+++ overlays/steam.nix
@@ -2,11 +2,12 @@
   steam = prev.steam.override {
     extraBwrapArgs = [
       "--bind $HOME/.local/share/steam_home $HOME"
+      "--bind $HOME/mounts $HOME/mounts"
       "--unsetenv XDG_CACHE_HOME"
       "--unsetenv XDG_CONFIG_HOME"
       "--unsetenv XDG_DATA_HOME"
       "--unsetenv XDG_STATE_HOME"
-    ]; 
+    ];
   };
 }
 
