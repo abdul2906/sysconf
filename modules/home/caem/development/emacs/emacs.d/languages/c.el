a936937ccf434fe6037dda7bc0092077901fa4e1
caem <me@caem.dev>
2025-02-02 16:42:11 +0100

    emacs: Add configuration for cc mode

---
+++ modules/home/caem/development/emacs/emacs.d/languages/c.el
@@ -0,0 +1,6 @@
+(setq c-basic-offset 4)
+
+;; https://files.catbox.moe/miu12k.jpg
+(setq c-default-style '((other . "stroustrup")
+                        (awk-mode . "awk")
+                        (java-mode . "java")))
